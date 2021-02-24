5fd355da149a0940d4ee597e51bee2b708716adb
saby175 <sabbir.khan175@gmail.com>
2021-02-25 04:44:54 +0530

    update

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
 <?php  
-echo "Hello World!";  
+$servername = "10.12.104.73";
+$username = "mydb";
+$password = "2jcWdlb7ShC7XNDi";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
 ?> 

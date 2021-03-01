7ee775008ba488d075f5605d6fae9c3751ea34f0
saby175 <sabbir.khan175@gmail.com>
2021-03-02 03:53:14 +0530

    update

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
-$servername = "192.168.0.6";
+$servername = "10.12.104.73";
 $username = "mydb";
 $password = "5vihKLtMfqO5HjAB";
+$dbname = "mydb";
 
 #Create connection
 $conn = mysqli_connect($servername, $username, $password);
@@ -10,5 +11,19 @@ $conn = mysqli_connect($servername, $username, $password);
 if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
 }
-echo "Connected successfully";
+
+$sql = "SELECT id, firstname, lastname FROM MyGuests";
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+  // output data of each row
+  while($row = mysqli_fetch_assoc($result)) {
+    echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+  }
+} else {
+  echo "0 results";
+}
+
+mysqli_close($conn);
 ?>
+
